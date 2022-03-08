5e762ebc1f58ee3db6ae9a696ece11832cde516d
liuxing <liuxing_cxzyzx@kedacom.com>
2022-03-08 13:22:41 +0800

    chat netty

--- src/main/java/com/learn/chat/NettyChatClientHandler.java
+++ src/main/java/com/learn/chat/NettyChatClientHandler.java
@@ -6,12 +6,7 @@ import io.netty.channel.SimpleChannelInboundHandler;
 public class NettyChatClientHandler extends SimpleChannelInboundHandler<String> {
 
     @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        ctx.writeAndFlush("客户端上线");
-    }
-
-    @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
-        System.out.println(s);
+        System.out.println("客户端收到消息：" + s);
     }
 }
--- src/main/java/com/learn/chat/NettyChatServerHandler.java
+++ src/main/java/com/learn/chat/NettyChatServerHandler.java
@@ -2,24 +2,37 @@ package com.learn.chat;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+import java.util.function.Predicate;
 
 public class NettyChatServerHandler extends SimpleChannelInboundHandler<String> {
 
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+    public static ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 
-        super.channelActive(ctx);
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("客户端：" + ctx.channel().remoteAddress() + "加入");
+        channels.add(ctx.channel());
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("客户端：" + ctx.channel().remoteAddress() + "离开");
+        channels.remove(ctx.channel());
+    }
 
-        System.out.println(String.format("服务端收到%s消息：%s", channelHandlerContext.channel().remoteAddress(), s));
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String s) throws Exception {
+        channels.stream().filter(Predicate.isEqual(ctx.channel()).negate()).forEach(channel -> {
+            channel.writeAndFlush(s);
+        });
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        cause.printStackTrace();
+//        cause.printStackTrace();
         ctx.close();
     }
 }
